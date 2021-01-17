0c877ac8eb6874868ada642456a7279c8a160ec5
Abdul Jaseem VP <67306692+vpjaseem@users.noreply.github.com>
2021-01-17 09:32:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sample HTML File</title>
+</head>
+<body>
+	<h1>Sample PHP Document</h1>
+	<p>This is a Sample PHP Template to learn basics of PHP</p>
+
+
+<!-- PHP Code Starts Here -->
+<?php
+
+echo nl2br("This is PHP Code! \n");
+$x=10;
+$y=20;
+$z=$x+$y;
+echo "Sum = ",$z;
+
+?>
+
+</body>
+</html>
